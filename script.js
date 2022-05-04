ae397a6c8143f26b2dd82836f298a9bddda4e81f
Vadim Karasik <vadkarasik@gmail.com>
2022-05-04 17:14:40 +0300

    refactor: add styles, edit script

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 function timer() {
-    var now = new Date();
-    var eventFinish = new Date(2020, 06, 10, 14, 31);
-    var diffTime = eventFinish.getTime() - now.getTime();
-    if(diffTime <= 0){
-        return false;
-    }
-    var s = Math.floor(diffTime / 1000);
-    var m = Math.floor(s / 60);
-    var h = Math.floor(m / 60);
-    var d = Math.floor(h / 24);
+  const now = new Date();
+  const eventFinish = new Date(2022, 05, 05, 12, 00);
+  const diffTime = eventFinish.getTime() - now.getTime();
+  if (diffTime <= 0) {
+    return false;
+  }
 
-    s = s % 60;
-    m = m % 60;
-    h = h % 24;
-    h = (h < 10) ? "0" + h : h;
-    s = (s < 10) ? "0" + s : s;
-    m = (m < 10) ? "0" + m : m;
+  let s = Math.floor(diffTime / 1000);
+  let m = Math.floor(s / 60);
+  let h = Math.floor(m / 60);
+  let d = Math.floor(h / 24);
 
-    var days = document.getElementById('days');
-    var hours = document.getElementById('hours');
-    var minutes = document.getElementById('minutes');
-    var seconds = document.getElementById('seconds');
-    hours.textContent = h;
-    hours.innerHTML = h;
-    minutes.textContent = m;
-    minutes.innerHTML = m;
-    seconds.textContent = s;
-    seconds.innerHTML = s;
-    setTimeout(timer,1000)
+  s = s % 60;
+  m = m % 60;
+  h = h % 24;
+  h = h < 10 ? '0' + h : h;
+  s = s < 10 ? '0' + s : s;
+  m = m < 10 ? '0' + m : m;
+
+  const days = document.getElementById('days');
+  const hours = document.getElementById('hours');
+  const minutes = document.getElementById('minutes');
+  const seconds = document.getElementById('seconds');
+  hours.innerHTML = h;
+  minutes.innerHTML = m;
+  seconds.innerHTML = s;
+  setTimeout(timer, 1000);
 }
 timer();
